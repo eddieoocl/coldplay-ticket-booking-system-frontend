ed82a62bf2f41874bf430458d392292d07630424
wonged6 <eddie.wong@oocl.com>
2024-12-13 12:21:23 +0800

    feat: hide my order when not logged in

--- src/app/components/navbar/NarbarMenu.tsx
+++ src/app/components/navbar/NarbarMenu.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
+import { useSession } from "next-auth/react";
 
 interface MenuItem {
     name: string;
@@ -21,32 +22,40 @@ const NavbarMenu: React.FC<NavbarMenuProps> = ({
     setActiveIndex,
     setIsMenuOpen,
 }) => {
+    const { data: session } = useSession();
     const { t } = useTranslation();
 
     return (
         <ul className="navbar-menu">
-            {menuItems.map((item, index) => (
-                <li key={index} className="navbar-list">
-                    <Link
-                        href={item.path}
-                        className={`navbar-link ${pathname === item.path ? "active" : ""}`}
-                        style={
-                            {
-                                "--active-color":
-                                    pathname === item.path
-                                        ? item.color
-                                        : "white",
-                            } as React.CSSProperties
-                        }
-                        onClick={() => {
-                            setActiveIndex(index);
-                            setIsMenuOpen(false);
-                        }}
-                    >
-                        {t(item.name)}
-                    </Link>
-                </li>
-            ))}
+            {menuItems
+                .map((item, index) => {
+                    if (item.name === "My order" && !session) {
+                        return null;
+                    }
+                    return (
+                        <li key={index} className="navbar-list">
+                            <Link
+                                href={item.path}
+                                className={`navbar-link ${pathname === item.path ? "active" : ""}`}
+                                style={
+                                    {
+                                        "--active-color":
+                                            pathname === item.path
+                                                ? item.color
+                                                : "white",
+                                    } as React.CSSProperties
+                                }
+                                onClick={() => {
+                                    setActiveIndex(index);
+                                    setIsMenuOpen(false);
+                                }}
+                            >
+                                {t(item.name)}
+                            </Link>
+                        </li>
+                    );
+                })
+                .filter((item) => item !== null)}
         </ul>
     );
 };
